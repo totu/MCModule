8bf9ea526724c037a5fffa840d5c416376eae9b8
totu <Totu@Totu-PC.(none)>
2013-02-25 20:28:20 +0200

    v0.1.15.1

--- c/index.php
+++ c/index.php
@@ -131,6 +131,7 @@
 							echo "<td style='text-align:left;'> 
 							<input id='id" . $counter . "' type='hidden' name='id' value='" . $c['id'] . "'>
 							<input id='t" . $counter . "' type='hidden' name='t' value='" . $c['title'] . "'>";
+							//change actions according to status
 							if ($c['status'] != 'sent' && $c['status'] != 'sending') {							
 								echo "<img title='Modify' class='mo' id='m" . $counter . "' src='../img/modify.png' width='30' alt='Modify'/>";
 								echo "<img title='Delete' class='mo' id='d" . $counter . "' src='../img/delete.png' width='30' alt='Delete'/>";
